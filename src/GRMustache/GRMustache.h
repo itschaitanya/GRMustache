4ea8c30953d92351481833941f8149185fcb44dd
Chaitanya Ramavajjala <chaitanyar@zillow.com>
2015-11-01 18:55:52 -0800

    Frameworks for GRMustache 6.3

---
+++ src/GRMustache/GRMustache.h
@@ -0,0 +1,31 @@
+//
+//  GRMustache.h
+//  GRMustache
+//
+//  Created by Chaitanya Ramavajjala on 10/23/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GRMustache.
+FOUNDATION_EXPORT double GRMustacheKitVersionNumber;
+
+//! Project version string for GRMustache.
+FOUNDATION_EXPORT const unsigned char GRMustacheKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GRMustache/PublicHeader.h>
+
+
+#import <GRMustache/GRMustacheConfiguration.h>
+#import <GRMustache/GRMustacheTemplateRepository.h>
+#import <GRMustache/GRMustacheTemplate.h>
+#import <GRMustache/GRMustacheTag.h>
+#import <GRMustache/GRMustacheContext.h>
+#import <GRMustache/GRMustacheFilter.h>
+#import <GRMustache/GRMustacheRendering.h>
+#import <GRMustache/GRMustacheTagDelegate.h>
+#import <GRMustache/GRMustache.h>
+#import <GRMustache/GRMustacheAvailabilityMacros.h>
+#import <GRMustache/GRMustacheError.h>
+#import <GRMustache/GRMustacheVersion.h>
